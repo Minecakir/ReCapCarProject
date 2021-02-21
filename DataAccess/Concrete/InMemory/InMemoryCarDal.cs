e2025c8da963690906be3e011cf63c1d3e6bd436
Mine ÇAKIR <152120181058@ogrenci.ogu.edu.tr>
2021-02-21 14:57:26 +0300

    First Step of Project
    Entities, DataAccess, Business and Console layers created. Also operations GetById, GetAll, Add, Update, Delete were written.

---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,54 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _car;
+        public InMemoryCarDal()
+        {
+            _car = new List<Car>
+            {
+                new Car{CarId=1,BrandId=1,ColorId=1,ModelYear=2015,DailyPrice=15000,Description="Black Mercedes"},
+                new Car{CarId=2,BrandId=1,ColorId=2,ModelYear=2019,DailyPrice=22000,Description="Red Mercedes"},
+                new Car{CarId=3,BrandId=2,ColorId=1,ModelYear=2020,DailyPrice=35000,Description="BMW"},
+                new Car{CarId=4,BrandId=3,ColorId=3,ModelYear=2020,DailyPrice=30000,Description="Wosvagen"},
+            };
+        }
+        public void Add(Car car)
+        {
+            _car.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _car.SingleOrDefault(p => p.CarId == car.CarId);
+            _car.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _car;
+        }
+
+        public List<Car> GetByCarId(int carId)
+        {
+            return _car.Where(p => p.CarId == carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _car.SingleOrDefault(p => p.CarId == car.CarId);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
